53a9c521fb0b5403e1deffad7f45e28cf687786b
pablo14 <pcasas.biz@gmail.com>
2020-08-27 18:59:40 -0300

    init and first release 0.1.0

---
+++ .gitignore.py
@@ -0,0 +1,18 @@
+syntax: glob
+.python-version
+.venv
+env/*
+venv/*
+ENV/*
+.idea/*
+.DS_Store
+dython.egg*/*
+*__pycache__*
+*run_stuff.py*
+build/*
+dist/*
+build_deploy.sh
+site/*
+debug.py
+AUX
+.ipynb_checkpoints
